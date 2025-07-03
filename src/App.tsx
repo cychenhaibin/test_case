d0b6ade0e1461cba1f567706eeeb8e117927760a
陈海彬 <3242477764@qq.com>
2025-07-04 01:38:51 +0800

    feat：修改为hash路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import AppRoutes from './route';
 
 const App: React.FC = () => (
-  <BrowserRouter>
+  <HashRouter>
     <AppRoutes />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
